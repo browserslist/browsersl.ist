3136f794a68c954378636843927c76a7550f5ae1
Jon Rohan <rohan@github.com>
2016-07-21 09:53:55 -0400

    Adding browser logos

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,26 @@
-const express = require('express');
-const router = express.Router();
-const browserslist = require('browserslist');
-const caniuse = require('caniuse-db/data.json').agents;
-const url = require('url')
+"use strict"
+
+const express = require('express')
+const router = express.Router()
+const browserslist = require('browserslist')
+const caniuse = require('caniuse-db/data.json').agents
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var query = req.query.q || "> 5%"
+router.get('/', function(req, res) {
+  const query = req.query.q || "> 5%"
+  let bl = null
   try {
-    var bl = browserslist(query);
-  } catch (e) { }
+    bl = browserslist(query)
+  } catch (e) {
+    // Error
+  }
 
-  var compatible = {}
+  const compatible = {}
 
   if (bl) {
-    bl.map( b => {
+    bl.map((b) => {
       b = b.split(" ")
-      var db = caniuse[b[0]]
+      const db = caniuse[b[0]]
 
       if(!compatible[db.type]) {
         compatible[db.type] = []
@@ -25,12 +29,13 @@ router.get('/', function(req, res, next) {
       compatible[db.type].push({
         "version": b[1],
         "id": b[0],
-        "name": db.browser
+        "name": db.browser,
+        "logo": "/images/" + b[0] + ".png"
       })
     })
   }
 
-  res.render('index', { compatible: compatible, query: query });
-});
+  res.render('index', { compatible: compatible, query: query })
+})
 
-module.exports = router;
+module.exports = router
