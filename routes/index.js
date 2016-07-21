4144959bf436a55cb7fc56fb6e9bbc85e34fdee8
Jon Rohan <rohan@github.com>
2016-07-21 08:34:25 -0400

    Updates

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,36 @@
-var express = require('express');
-var router = express.Router();
-var browserslist = require('browserslist');
+const express = require('express');
+const router = express.Router();
+const browserslist = require('browserslist');
+const caniuse = require('caniuse-db/data.json').agents;
+const url = require('url')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var browsers = browserslist(req.query.q);
-  res.render('index', { title: 'Express' });
+  var query = req.query.q || "> 5%"
+  try {
+    var bl = browserslist(query);
+  } catch (e) { }
+
+  var compatible = {}
+
+  if (bl) {
+    bl.map( b => {
+      b = b.split(" ")
+      var db = caniuse[b[0]]
+
+      if(!compatible[db.type]) {
+        compatible[db.type] = []
+      }
+
+      compatible[db.type].push({
+        "version": b[1],
+        "id": b[0],
+        "name": db.browser
+      })
+    })
+  }
+
+  res.render('index', { compatible: compatible, query: query });
 });
 
 module.exports = router;
