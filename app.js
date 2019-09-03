655067bb2fa3ac6b2814448982b4643d0f4cd8e4
XhmikosR <xhmikosr@gmail.com>
2019-09-03 21:12:56 +0300

    Remove the unused serve-favicon package.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 const express = require('express')
 const path = require('path')
-const favicon = require('serve-favicon')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
@@ -15,7 +14,6 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
