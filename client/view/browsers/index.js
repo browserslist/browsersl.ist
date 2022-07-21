7294981396f8ea61b45b69f3e2f409e23925a326
Alexander Chabin <nibach@ya.ru>
2022-07-21 18:52:37 +0500

    Add test JSON data for client

--- client/view/browsers/index.js
+++ client/view/browsers/index.js
@@ -1,103 +1,25 @@
-import wikipediaLinks from '../../data/wikipedia-links.js'
-import regions from '../../data/regions.js'
+import fakeData from '../../__fixtures/query-default.json';
 
-// TODO Put client and server to the single Docker image to use the same domain and re-use the power of HTTP/2
-const API_HOST = 'http://localhost:5000/api'
-const WIKIPEDIA_URL = 'https://en.wikipedia.org/wiki/'
+document.getElementById('browsers-root').innerHTML = `
+  <ul class="browsers">
 
-let input = document.getElementById('browsers-input')
-let select = document.getElementById('browsers-region-select')
+      ${fakeData.browsers.map(({ name, link, icon, versions }) => `
+        <li class="browsers__item">
+           <img src="${icon}" alt="" />
+           <a href="${link}" target="_blank" rel="noreferrer noopener">${name}</a>
 
-async function init() {
-  renderResults(await getBrowsers())
+           <ul>
+              ${versions.map(({ inQuery, version, coverage }) => `
+                <li ${!inQuery ? 'style="color: gray' : ''}">
+                  ${version} — ${Math.floor(coverage * 1000) / 1000}%
+                </li>
+              `).join("")}
+            </ul>
+        </li>
+        `).join("")}
+  </ul>
 
-  let { continentsOptgroup, countriesOptgroup } =
-    renderRegionsOptgroups(regions)
-  select.appendChild(continentsOptgroup)
-  select.appendChild(countriesOptgroup)
-}
-
-init()
-
-input.addEventListener('input', async () => {
-  // TODO debounce 100ms in printing (too much requests)
-  // TODO abort previous requests
-  renderResults(await getBrowsers(input.value, select.value))
-})
-
-select.addEventListener('change', async () => {
-  renderResults(await getBrowsers(input.value, select.value))
-})
-
-async function getBrowsers(query = 'defaults', region = 'Global') {
-  // TODO Handling errors: 400 status and connection errors
-  let response = await fetch(
-    `${API_HOST}/browsers?q=${encodeURIComponent(
-      query
-    )}&region=${encodeURIComponent(region)}`
-  )
-  return await response.json()
-}
-
-function renderResults({ browsers, versions: { browserslist, caniuse } }) {
-  document.getElementById('browsers-root').innerHTML = `
-    <ul class="browsers">
-        ${browsers
-          .map(
-            ({ id, name, versions }) => `
-          <li class="browsers__item">
-            <img src="/${id}.png" alt="" />
-            <a href="${getWikipediaLink(
-              id
-            )}" target="_blank" rel="noreferrer noopener">${name}</a>
-
-            <ul>
-                ${Object.entries(versions)
-                  .sort(([, coverageA], [, coverageB]) => coverageB - coverageA)
-                  .map(
-                    ([version, coverage]) => `
-                  <li>
-                    ${version} — ${coverage}%
-                  </li>`
-                  )
-                  .join('')}
-              </ul>
-          </li>
-          `
-          )
-          .join('')}
-    </ul>
-
-    Browserslist ver: ${browserslist}
-    <br />
-    Data provided by caniuse-db: ${caniuse}
-    `
-}
-
-function renderRegionsOptgroups({ continents, countries }) {
-  let renderOption = (id, name) => {
-    let option = document.createElement('option')
-    option.value = id
-    option.innerHTML = name
-    return option
-  }
-
-  let createOptgroup = (groupName, regionsGroup) => {
-    let optgroup = document.createElement('optgroup')
-    optgroup.label = groupName
-    for (let { id, name } of regionsGroup) {
-      let option = renderOption(id, name)
-      optgroup.appendChild(option)
-    }
-    return optgroup
-  }
-
-  return {
-    continentsOptgroup: createOptgroup('Continents', continents),
-    countriesOptgroup: createOptgroup('Countries', countries)
-  }
-}
-
-function getWikipediaLink(id) {
-  return WIKIPEDIA_URL + wikipediaLinks[id]
-}
+  Browserslist ver: ${fakeData.browserslistVersion}
+  <br />
+  Data provided by caniuse-db: ${fakeData.caniuseVersion}
+`;
