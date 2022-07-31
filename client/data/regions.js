4076d8802e5c892e11a3b007701cae0498f83bd8
Alexander Chabin <nibach@ya.ru>
2022-07-31 18:50:36 +0500

    Create `regions.json` on client's `postinstall` step (#298)
    * Moved the client's data to `client/data`
    
    * Add caniuse-lite to the client as devDependency
    
    * Add postinstall step for generate `client/data/regions.json`
    
    * Temporarily handled the ESLint error
    
    * Remove `--ignore-scripts` for `postinstall` working from CI
    
    * Remove unused wikipedia-links.json
    
    * Show creating `region.json` message without `console.info`
    
    * Restored `--ignore-scripts` flag for security (CI)
    
    * Add `<select>` control for region. Fix .size-limit test (#299)
    
    * Add `<select>` control for region in the client
    
    * Get country names from from `window.Intl` (js 5.46kb → 4.46 kB)
    
    * Remove unused DOMContentLoaded handler
    
    * Added an explicit `build:regions` script

---
+++ client/data/regions.js
@@ -0,0 +1,26 @@
+// regions.json is created on `postinstall` step
+import { continents, countryCodes } from './regions.json'
+
+export default {
+  continents: Object.entries(continents).map(([id, name]) => {
+    return {
+      id,
+      name
+    }
+  }),
+  countries: countryCodes.map(id => {
+    return {
+      id,
+      name: getCountryName(id)
+    }
+  })
+}
+
+function getCountryName(id) {
+  let isIntlDisplayNameSupports = 'Intl' in window && 'DisplayNames' in window.Intl
+
+  // Show country `id` instead country fullname for old browsers
+  return isIntlDisplayNameSupports
+    ? new Intl.DisplayNames(['us'], { type: 'region' }).of(id)
+    : id
+}
