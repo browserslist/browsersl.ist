16563f11a08fe5426e72da0b45f4f388abc321cd
Alexander Chabin <nibach@ya.ru>
2022-07-31 14:06:41 +0500

    Moved the client's data to `client/data`

---
+++ client/data/regions.js
@@ -0,0 +1,7 @@
+// regions.json is created on `postinstall` step
+import { continents, countries } from './regions.json'
+
+export default {
+  continents,
+  countries
+}
--- client/view/browsers/index.js
+++ client/view/browsers/index.js
@@ -1,4 +1,5 @@
-import wikipediaLinks from './wikipedia-links.json'
+import wikipediaLinks from '../../data/wikipedia-links.js'
+import regions from '../../data/regions.js'
 
 // TODO Put client and server to the single Docker image to use the same domain and re-use the power of HTTP/2
 const API_HOST = 'http://localhost:5000/api/browsers'
