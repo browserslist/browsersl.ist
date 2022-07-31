1741a47896403cfa51c6db43448f1a460b936b8e
Alexander Chabin <nibach@ya.ru>
2022-08-01 04:35:20 +0500

    Migrate tests from `uvu` to `node:test`

--- server/test/app.test.js
+++ server/test/app.test.js
@@ -1,53 +1,50 @@
-import { match } from 'assert'
+import test from 'node:test'
+import { equal, match } from 'assert'
 import { URL } from 'url'
-import { test } from 'uvu'
-import { equal } from 'uvu/assert'
 
 import App from '../app.js'
 
 const base = `http://localhost:${App.address().port}/api/`
 
-test('responses `defauts` query for `/browsers` route without `q` param', async () => {
-  let url = new URL(`browsers`, base)
-  let response = await fetch(url)
-  let data = await response.json()
-  equal(data.query, 'defaults')
-})
+test('Integration tests', async (t) => {
+  await t.test('responses `defauts` query for `/browsers` route without `q` param', async () => {
+    let url = new URL(`browsers`, base)
+    let response = await fetch(url)
+    let data = await response.json()
+    equal(data.query, 'defaults')
+  })
 
-test('responses `Global` region for `/browsers` route without `region` param', async () => {
-  let url = new URL(`browsers`, base)
-  let response = await fetch(url)
-  let data = await response.json()
-  equal(data.region, 'Global')
-})
+  await t.test('responses `Global` region for `/browsers` route without `region` param', async () => {
+    let url = new URL(`browsers`, base)
+    let response = await fetch(url)
+    let data = await response.json()
+    equal(data.region, 'Global')
+  })
 
-test('responses status 200 for `/browsers` route', async () => {
-  let url = new URL(`browsers`, base)
-  let response = await fetch(url)
-  equal(response.status, 200)
-})
+  await t.test('responses status 200 for `/browsers` route', async () => {
+    let url = new URL(`browsers`, base)
+    let response = await fetch(url)
+    equal(response.status, 200)
+  })
 
-test('responses 400 for `/browsers` route with wrong `q` param', async () => {
-  let url = new URL(`browsers?q=wrong-query`, base)
-  let response = await fetch(url)
-  let error = await response.json()
+  await t.test('responses 400 for `/browsers` route with wrong `q` param', async () => {
+    let url = new URL(`browsers?q=wrong-query`, base)
+    let response = await fetch(url)
+    let error = await response.json()
 
-  equal(response.status, 400)
-  match(error.message, /Unknown/)
-})
+    equal(response.status, 400)
+    match(error.message, /Unknown/)
+  })
 
-test('responses 404 for unknown route', async () => {
-  let url = new URL(`wrong-route`, base)
-  let response = await fetch(url)
-  let error = await response.json()
+  await t.test('responses 404 for unknown route', async () => {
+    let url = new URL(`wrong-route`, base)
+    let response = await fetch(url)
+    let error = await response.json()
 
-  equal(response.status, 404)
-  match(error.message, /Not found/)
-})
+    equal(response.status, 404)
+    match(error.message, /Not found/)
+  })
 
-test.after(() => {
   App.closeAllConnections()
   App.close()
 })
-
-test.run()
