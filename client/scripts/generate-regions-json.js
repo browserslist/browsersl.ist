ec387b28909595de9c6903b3d5139b0f4392135f
Alexander Chabin <nibach@ya.ru>
2022-07-31 14:10:09 +0500

    Add postinstall step for generate `client/data/regions.json`

---
+++ client/scripts/generate-regions-json.js
@@ -0,0 +1,41 @@
+import fs from 'fs'
+
+const DATA_REGION_FILE = 'data/regions.json'
+const REGIONS_LIST_PATH = 'node_modules/caniuse-lite/data/regions'
+
+const regions = {
+  continents: {
+    'alt-ww': 'Global',
+    'alt-af': 'Africa',
+    'alt-an': 'Antarctica',
+    'alt-as': 'Asia',
+    'alt-eu': 'Europe',
+    'alt-na': 'North America',
+    'alt-oc': 'Oceania',
+    'alt-sa': 'South America'
+  },
+  countries: getCaniuseCountries()
+}
+
+fs.writeFileSync(DATA_REGION_FILE, JSON.stringify(regions))
+// eslint-disable-next-line no-console
+console.info(`A file "client/${DATA_REGION_FILE}" with regions has been created`);
+
+function getCaniuseCountries() {
+  let regionCodes = fs
+    .readdirSync(REGIONS_LIST_PATH)
+    .map(file => file.split('.js')[0])
+
+  let countries = regionCodes
+    .filter(regionCode => {
+      let isContinentCode = regionCode.includes('alt-')
+      return !isContinentCode
+    })
+    .sort((a, b) => b - a)
+    .map(countryCode => [
+      countryCode,
+      new Intl.DisplayNames(['us'], { type: 'region' }).of(countryCode)
+    ])
+
+  return Object.fromEntries(countries)
+}
