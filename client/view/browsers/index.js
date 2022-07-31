ff5395ea387fb061779a4626ed73ad8c63b4e400
Alexander Chabin <nibach@ya.ru>
2022-07-31 18:50:36 +0500

    Create `regions.json` on client's `postinstall` step (#298)
    * Moved the client's data to `client/data`
    
    * Add caniuse-lite to the client as devDependency
    
    * Add postinstall step for generate `client/data/regions.json`
    
    * Temporarily handled the ESLint error
    
    * Remove `--ignore-scripts` for `postinstall` working from CI
    
    * Remove unused wikipedia-links.json
    
    * Show creating `region.json` message without `console.info`
    
    * Restored `--ignore-scripts` flag for security (CI)
    
    * Add `<select>` control for region. Fix .size-limit test (#299)
    
    * Add `<select>` control for region in the client
    
    * Get country names from from `window.Intl` (js 5.46kb → 4.46 kB)
    
    * Remove unused DOMContentLoaded handler
    
    * Added an explicit `build:regions` script

--- client/view/browsers/index.js
+++ client/view/browsers/index.js
@@ -1,26 +1,45 @@
-import wikipediaLinks from './wikipedia-links.json'
+import wikipediaLinks from '../../data/wikipedia-links.js'
+import regions from '../../data/regions.js'
 
 // TODO Put client and server to the single Docker image to use the same domain and re-use the power of HTTP/2
-const API_HOST = 'http://localhost:5000/api/browsers'
+const API_HOST = 'http://localhost:5000/api'
 const WIKIPEDIA_URL = 'https://en.wikipedia.org/wiki/'
 
-document.getElementById('browsers-input').addEventListener('input', () => {
-  let query = document.getElementById('browsers-input').value
+let input = document.getElementById('browsers-input')
+let select = document.getElementById('browsers-region-select')
+
+async function init() {
+  renderResults(await getBrowsers())
+
+  let { continentsOptgroup, countriesOptgroup } =
+    renderRegionsOptgroups(regions)
+  select.appendChild(continentsOptgroup)
+  select.appendChild(countriesOptgroup)
+}
+
+init()
+
+input.addEventListener('input', async () => {
   // TODO debounce 100ms in printing (too much requests)
   // TODO abort previous requests
-  sendQuery(query)
+  renderResults(await getBrowsers(input.value, select.value))
 })
 
-sendQuery('defaults')
+select.addEventListener('change', async () => {
+  renderResults(await getBrowsers(input.value, select.value))
+})
 
-async function sendQuery(query) {
+async function getBrowsers(query = 'defaults', region = 'Global') {
   // TODO Handling errors: 400 status and connection errors
-  let response = await fetch(`${API_HOST}?q=${encodeURIComponent(query)}`)
-  let {
-    browsers,
-    versions: { browserslist, caniuse }
-  } = await response.json()
+  let response = await fetch(
+    `${API_HOST}/browsers?q=${encodeURIComponent(
+      query
+    )}&region=${encodeURIComponent(region)}`
+  )
+  return await response.json()
+}
 
+function renderResults({ browsers, versions: { browserslist, caniuse } }) {
   document.getElementById('browsers-root').innerHTML = `
     <ul class="browsers">
         ${browsers
@@ -53,6 +72,30 @@ async function sendQuery(query) {
     <br />
     Data provided by caniuse-db: ${caniuse}
     `
+}
+
+function renderRegionsOptgroups({ continents, countries }) {
+  let renderOption = (id, name) => {
+    let option = document.createElement('option')
+    option.value = id
+    option.innerHTML = name
+    return option
+  }
+
+  let createOptgroup = (groupName, regionsGroup) => {
+    let optgroup = document.createElement('optgroup')
+    optgroup.label = groupName
+    for (let { id, name } of regionsGroup) {
+      let option = renderOption(id, name)
+      optgroup.appendChild(option)
+    }
+    return optgroup
+  }
+
+  return {
+    continentsOptgroup: createOptgroup('Continents', continents),
+    countriesOptgroup: createOptgroup('Countries', countries)
+  }
 }
 
 function getWikipediaLink(id) {
