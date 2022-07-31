dabab99388f01c5ad75cc804396890dd646e3e68
Alexander Chabin <nibach@ya.ru>
2022-07-31 18:33:05 +0500

    Add `<select>` control for region. Fix .size-limit test (#299)
    * Add `<select>` control for region in the client
    
    * Get country names from from `window.Intl` (js 5.46kb → 4.46 kB)
    
    * Remove unused DOMContentLoaded handler

--- client/data/regions.js
+++ client/data/regions.js
@@ -1,7 +1,26 @@
 // regions.json is created on `postinstall` step
-import { continents, countries } from './regions.json'
+import { continents, countryCodes } from './regions.json'
 
 export default {
-  continents,
-  countries
+  continents: Object.entries(continents).map(([id, name]) => {
+    return {
+      id,
+      name
+    }
+  }),
+  countries: countryCodes.map(id => {
+    return {
+      id,
+      name: getCountryName(id)
+    }
+  })
+}
+
+function getCountryName(id) {
+  let isIntlDisplayNameSupports = 'Intl' in window && 'DisplayNames' in window.Intl
+
+  // Show country `id` instead country fullname for old browsers
+  return isIntlDisplayNameSupports
+    ? new Intl.DisplayNames(['us'], { type: 'region' }).of(id)
+    : id
 }
