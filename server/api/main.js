6132f622e61777a8b81146ac137ab0f26eec0f79
Alexander Chabin <nibach@ya.ru>
2022-08-08 03:36:42 +0500

    Сaching in memory `index.html` for 3 min

--- server/api/main.js
+++ server/api/main.js
@@ -1,19 +1,36 @@
 import { readFile } from 'fs'
 import { URL } from 'node:url'
 
+const CACHE_TIMEOUT = 3 * 60 * 1000
+
+let responseCache = null
+
+setInterval(() => {
+  responseCache = null
+}, CACHE_TIMEOUT)
+
 export default async function handleMain(req, res) {
   let filePath = new URL(`../../client/dist/index.html`, import.meta.url)
 
-  readFile(filePath, (error, content) => {
-    if (!error) {
-      res.writeHead(200, {
-        'Content-Type': 'text/html',
-        'Cache-Control': 'public, max-age=0, must-revalidate'
-      })
-      res.end(content, 'utf-8')
-    } else {
-      res.writeHead(500)
-      res.end('500 Internal Server Error')
-    }
-  })
+  let sendResponse = content => {
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Cache-Control': 'public, max-age=0, must-revalidate'
+    })
+    res.end(content, 'utf-8')
+  }
+
+  if (responseCache) {
+    sendResponse(responseCache)
+  } else {
+    readFile(filePath, (error, content) => {
+      if (!error) {
+        sendResponse(content)
+        responseCache = content
+      } else {
+        res.writeHead(500)
+        res.end('500 Internal Server Error')
+      }
+    })
+  }
 }
