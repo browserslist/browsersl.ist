38b79a4d3c13dc4e75713468d3290489efa73547
Sofi Valitova <ariarzer@gmail.com>
2022-08-09 17:28:52 +0300

    remove initialization functions (close #323) (#326)

--- client/view/QueryLink/queryLink.js
+++ client/view/QueryLink/queryLink.js
@@ -1,11 +1,9 @@
 import { submitForm } from '../Form/form.js'
 
-export function initQueryLinks() {
-  let links = document.querySelectorAll('a[data-query]')
-  links.forEach(item => {
-    item.addEventListener('click', e => {
-      e.preventDefault()
-      submitForm(item.getAttribute('data-query'))
-    })
+let links = document.querySelectorAll('a[data-query]')
+links.forEach(item => {
+  item.addEventListener('click', e => {
+    e.preventDefault()
+    submitForm(item.getAttribute('data-query'))
   })
-}
+})
