c8076e2e16017467be416c5978252e868837f504
Alexander Chabin <nibach@ya.ru>
2022-08-10 21:54:34 +0500

    Add `node:` to imports

--- server/lib/get-file-data.js
+++ server/lib/get-file-data.js
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import path from 'path'
+import fs from 'node:fs'
+import path from 'node:path'
 
 export default function getFileData(filePath) {
   let { name, ext } = path.parse(filePath.pathname)
