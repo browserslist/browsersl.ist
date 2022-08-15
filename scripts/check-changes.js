48092627fe3e45207b205ee84c20d346b3e9ec61
Andrey Sitnik <andrey@sitnik.ru>
2022-08-15 20:10:38 +0200

    Fix auto-update

--- scripts/check-changes.js
+++ scripts/check-changes.js
@@ -1,5 +1,5 @@
 import { execSync } from 'node:child_process'
 
-if (execSync('git status pnpm-lock.yaml').includes('?? pnpm-lock.yaml')) {
+if (execSync('git status pnpm-lock.yaml -s').includes(' pnpm-lock.yaml')) {
   process.stdout.write('::set-output name=changes::1\n')
 }
