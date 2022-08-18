d9f122f6f8e61641cb7c9d715890691ecbc58e45
Alexander Chabin <nibach@ya.ru>
2022-08-18 08:06:23 +0500

    Disable quotes removing from `query` on server

--- server/handlers/api-browsers.js
+++ server/handlers/api-browsers.js
@@ -10,12 +10,10 @@ export default async function handleAPIBrowsers(req, res) {
   let { searchParams: params } = new URL(req.url, `http://${req.headers.host}/`)
 
   let query = params.get('q') || QUERY_DEFAULTS
-  let queryWithoutQuotes = query.replace(/'|"/g, '')
-
   let region = params.get('region') || REGION_GLOBAL
 
   try {
-    sendResponseAPI(res, 200, await getBrowsers(queryWithoutQuotes, region))
+    sendResponseAPI(res, 200, await getBrowsers(query, region))
   } catch (error) {
     sendResponseAPI(res, 400, { message: error.message })
   }
