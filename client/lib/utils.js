76f5fa83cd8cc2e7ea1767046821e279da317b66
Andrey Sitnik <andrey@sitnik.ru>
2022-08-25 07:40:57 +0200

    Clean up HTML generation

---
+++ client/lib/utils.js
@@ -0,0 +1,19 @@
+export function debounce(callback, delay) {
+  let timeout
+  return function () {
+    clearTimeout(timeout)
+    timeout = setTimeout(callback, delay)
+  }
+}
+
+export function formatPercent(percent) {
+  let rounded = percent < 1 ? percent.toFixed(2) : percent.toFixed(1)
+  return rounded + ' %'
+}
+
+export function createTag(tag, classes = [], text = undefined) {
+  let el = document.createElement(tag)
+  el.classList.add(...classes)
+  if (text) el.innerText = text
+  return el
+}
--- client/view/Browsers/Browsers.js
+++ client/view/Browsers/Browsers.js
@@ -1,114 +1,91 @@
 import wikipediaLinks from '../../data/wikipedia-links.js'
 import * as browsersIcons from '../../data/browsers-logos.js'
+import { formatPercent, createTag } from '../../lib/utils.js'
 
 let container = document.querySelector('[data-id=browsers]')
-let results = document.querySelector('[data-id=browsers_results]')
+let table = document.querySelector('[data-id=browsers_table]')
 
-export function formatPercent(percent) {
-  let rounded = percent < 1 ? percent.toFixed(2) : percent.toFixed(1)
-  return rounded + ' %'
-}
-
-export function toggleShowStats(isShown) {
+export function toggleBrowsers(isShown) {
   container.hidden = !isShown
 }
 
-function createCoverageCell(coverage) {
-  let coveragePercentageHtmlString = cov =>
-    cov !== null ? formatPercent(cov) : '—'
-  let coveragePercentageCssString = cov => {
-    let result = (Math.log(1 + cov) * 100) / Math.log(1 + 100)
-    if (result === 0) {
-      return '0'
-    } else if (result > 5) {
-      return result + '%'
-    }
-    return '1px'
+let coverageCss = cov => {
+  let result = (Math.log(1 + cov) * 100) / Math.log(1 + 100)
+  if (result === 0) {
+    return '0'
+  } else if (result > 5) {
+    return result + '%'
   }
+  return '1px'
+}
 
-  let coverageCell = document.createElement('td')
-  coverageCell.classList.add('Browsers_cell')
-  coverageCell.innerText = coveragePercentageHtmlString(coverage)
-  coverageCell.classList.add('is-coverage')
-
-  coverageCell.style.setProperty(
-    '--coverage',
-    coveragePercentageCssString(coverage)
+function createCoverageCell(coverage) {
+  let cell = createTag(
+    'td',
+    ['Browsers_cell', 'is-coverage'],
+    coverage !== null ? formatPercent(coverage) : '—'
   )
-  return coverageCell
+  cell.style.setProperty('--coverage', coverageCss(coverage))
+  return cell
 }
 
 function createVersionCell(version) {
-  let versionCell = document.createElement('td')
-  versionCell.classList.add('Browsers_cell')
-  versionCell.innerText = version
-  return versionCell
+  return createTag('td', ['Browsers_cell'], version)
+}
+
+function createLine(...items) {
+  let tr = createTag('tr', ['Browsers_line'])
+  tr.replaceChildren(...items)
+  return tr
 }
 
 export function updateBrowsersStats(data) {
-  let table = document.createElement('table')
-  table.classList.add('Browsers_table')
+  table.replaceChildren(
+    ...data.map(({ id, name, versions: versionsInput }) => {
+      let versions = Object.entries(versionsInput)
+        .sort(([versionA], [versionB]) => versionB - versionA)
+        .map(([version, coverage]) => {
+          return {
+            version,
+            coverage
+          }
+        })
 
-  data.forEach(({ id, name, versions: versionsInput }) => {
-    let versions = Object.entries(versionsInput)
-      .sort(([versionA], [versionB]) => versionB - versionA)
-      .map(([version, coverage]) => {
-        return {
-          version,
-          coverage
-        }
+      let iconCell = createTag('td', ['Browsers_cell'])
+      iconCell.setAttribute('rowspan', versions.length)
+      if (id in browsersIcons) {
+        let iconElem = createTag('img', ['Browsers_icon'])
+        iconElem.src = browsersIcons[id]
+        iconElem.setAttribute('alt', '')
+        iconCell.appendChild(iconElem)
+      }
+
+      let nameCell = createTag('td', ['Browsers_cell'])
+      let nameLink = createTag('a', ['Link'])
+      // TODO Need to take care of the case when we do not have link for some browser. Can I Use sometimes adds browsers
+      nameLink.href = wikipediaLinks[id]
+      nameLink.rel = 'noreferrer noopener'
+      nameLink.target = '_blank'
+      nameCell.setAttribute('rowspan', versions.length)
+      nameLink.innerText = name
+      nameCell.appendChild(nameLink)
+
+      let body = createTag('tbody', ['Browsers_body'])
+      body.appendChild(
+        createLine(
+          iconCell,
+          nameCell,
+          createVersionCell(versions[0].version),
+          createCoverageCell(versions[0].coverage)
+        )
+      )
+      versions.slice(1).forEach(({ version, coverage }) => {
+        body.appendChild(
+          createLine(createVersionCell(version), createCoverageCell(coverage))
+        )
       })
 
-    let tBody = document.createElement('tbody')
-    tBody.classList.add('Browsers_body')
-    let tr = document.createElement('tr')
-    tr.classList.add('Browsers_line')
-
-    let iconCell = document.createElement('td')
-    iconCell.classList.add('Browsers_cell')
-    iconCell.setAttribute('rowspan', versions.length)
-    tr.appendChild(iconCell)
-
-    if (id in browsersIcons) {
-      let iconElem = document.createElement('img')
-      iconElem.classList.add('Browsers_icon')
-      iconElem.src = browsersIcons[id]
-      iconElem.setAttribute('alt', '')
-      iconCell.appendChild(iconElem)
-    }
-
-    let nameCell = document.createElement('td')
-    nameCell.classList.add('Browsers_cell')
-    let nameLink = document.createElement('a')
-    nameLink.classList.add('Link')
-    // TODO Need to take care of the case when we do not have link for some browser. Can I Use sometimes adds browsers
-    nameLink.href = wikipediaLinks[id]
-    nameLink.rel = 'noreferrer noopener'
-    nameLink.target = '_blank'
-    nameCell.setAttribute('rowspan', versions.length)
-    nameLink.innerText = name
-    nameCell.appendChild(nameLink)
-    tr.appendChild(nameCell)
-
-    tr.appendChild(createVersionCell(versions[0].version))
-
-    tr.appendChild(createCoverageCell(versions[0].coverage))
-
-    tBody.appendChild(tr)
-
-    versions.slice(1).forEach(item => {
-      let { version, coverage } = item
-      let versionTr = document.createElement('tr')
-      versionTr.classList.add('Browsers_line')
-
-      versionTr.appendChild(createVersionCell(version))
-
-      versionTr.appendChild(createCoverageCell(coverage))
-
-      tBody.appendChild(versionTr)
+      return body
     })
-    table.appendChild(tBody)
-  })
-
-  results.replaceChildren(table)
+  )
 }
