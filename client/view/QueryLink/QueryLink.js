830fd6b53b86f14b81f607cf30f8a650cdf1051b
Alexander Chabin <nibach@ya.ru>
2022-08-27 14:22:11 +0500

    Update region in links (#469)
    * Update region in `queryLink`
    
    * Add `encodeURI` to `queryLink` href

--- client/view/QueryLink/QueryLink.js
+++ client/view/QueryLink/QueryLink.js
@@ -1,20 +1,40 @@
+import { DEFAULT_REGION } from '../../data/regions.js'
 import { setFormValues, submitForm } from '../Form/Form.js'
 import { scrollToInteractive } from '../Interactive/Interactive.js'
 
 let links = document.querySelectorAll('a.QueryLink')
 
-links.forEach(item => {
-  let queryAttr = item.getAttribute('data-query')
-  let query = queryAttr || item.textContent.trim()
+for (let link of links) {
+  let queryAttr = link.getAttribute('data-query')
+  let query = queryAttr || link.textContent.trim()
 
-  item.setAttribute('href', `?q=${query}`)
+  link.href = '?' + new URLSearchParams({ q: query })
 
-  item.addEventListener('click', e => {
+  link.addEventListener('click', e => {
     e.preventDefault()
+    let region = new URL(link.href).searchParams.get('region')
 
-    setFormValues({ query })
-
+    setFormValues({ query, region })
     submitForm()
     scrollToInteractive()
   })
-})
+}
+
+export function updateQueryLinksRegion(region) {
+  for (let link of links) {
+    let linkParams = new URL(link.href).searchParams
+    let prevRegion = linkParams.get('region')
+
+    if (region === prevRegion) {
+      return
+    }
+
+    if (region !== DEFAULT_REGION) {
+      linkParams.set('region', region)
+    } else {
+      linkParams.delete('region')
+    }
+
+    link.setAttribute('href', '?' + linkParams)
+  }
+}
