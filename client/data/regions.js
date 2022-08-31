b3858f2ffefaee6c610a3aa094e0d52897668702
Andrey Sitnik <andrey@sitnik.ru>
2022-08-31 15:29:02 +0200

    Be ready for lack of Intl

--- client/data/regions.js
+++ client/data/regions.js
@@ -5,13 +5,15 @@ export const DEFAULT_REGION = 'alt-ww'
 
 export const regionList = [...Object.keys(continents), ...countryCodes]
 
-let comparer = new Intl.Collator('en-US')
-
-let getCountryName
+let getCountryName, sorter
 if ('Intl' in window && 'DisplayNames' in window.Intl) {
   let namer = new Intl.DisplayNames('en-US', { type: 'region' })
+  sorter = new Intl.Collator('en-US')
   getCountryName = id => namer.of(id)
 } else {
+  sorter = {
+    compare: () => 0
+  }
   getCountryName = id => id
 }
 
@@ -19,5 +21,5 @@ export const regionGroups = {
   continents: Object.entries(continents).map(([id, name]) => ({ id, name })),
   countries: countryCodes
     .map(id => ({ id, name: getCountryName(id) }))
-    .sort((a, b) => comparer.compare(a.name, b.name))
+    .sort((a, b) => sorter.compare(a.name, b.name))
 }
