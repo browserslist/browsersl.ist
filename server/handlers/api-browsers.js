e01b5a2380c28322163bf47aab9fc5cc44dc5223
Andrey Sitnik <andrey@sitnik.ru>
2022-09-02 19:01:13 +0200

    Re-use URL

--- server/handlers/api-browsers.js
+++ server/handlers/api-browsers.js
@@ -1,14 +1,9 @@
-import { URL } from 'node:url'
-
 import { sendResponseAPI } from '../lib/send-response.js'
 import { getBrowsers } from '../lib/get-browsers.js'
 
-export async function handleAPIBrowsers(req, res) {
-  let { searchParams: params } = new URL(req.url, `http://${req.headers.host}/`)
-
-  let query = params.get('q') || 'defaults'
-  let region = params.get('region') || 'alt-ww'
-
+export async function handleAPIBrowsers(req, res, url) {
+  let query = url.searchParams.get('q') || 'defaults'
+  let region = url.searchParams.get('region') || 'alt-ww'
   try {
     sendResponseAPI(res, 200, await getBrowsers(query, region))
   } catch (error) {
