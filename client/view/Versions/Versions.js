7145192b91ff512cf4837b37f3be47ae9a778fdf
Andrey Sitnik <andrey@sitnik.ru>
2023-01-31 13:46:39 +0400

    Improve date

--- client/view/Versions/Versions.js
+++ client/view/Versions/Versions.js
@@ -2,12 +2,12 @@ let updated = document.querySelector('[data-id=versions_updated]')
 let browserslist = document.querySelector('[data-id=versions_browserslist]')
 let caniuse = document.querySelector('[data-id=versions_caniuse]')
 
-export function updateVersions(
-  browserslistVersion,
-  caniuseVersion,
-  updatedDate
-) {
-  updated.innerText = new Date(updatedDate).toLocaleDateString('uk')
+export function updateVersions(browserslistVersion, caniuseVersion, updatedMs) {
+  updated.innerText = new Date(updatedMs).toLocaleDateString('en-UK', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric'
+  })
   browserslist.innerText = browserslistVersion
   caniuse.innerText = caniuseVersion
 }
