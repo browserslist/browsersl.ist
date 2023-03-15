454c25833cc4c431c4ca7c9552147dadccecbaf0
Andrey Sitnik <andrey@sitnik.ru>
2023-03-15 23:52:04 +0100

    Update to new Github Actions output API

--- scripts/check-changes.js
+++ scripts/check-changes.js
@@ -1,5 +1,6 @@
+import { writeSync } from 'node:fs/promises'
 import { execSync } from 'node:child_process'
 
 if (execSync('git status pnpm-lock.yaml -s').includes(' pnpm-lock.yaml')) {
-  process.stdout.write('::set-output name=changes::1\n')
+  writeSync(process.env.GITHUB_OUTPUT, 'changes=1\n')
 }
