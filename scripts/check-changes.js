ef6ce215f78c0b5856bfd44cea006f8fe7e658db
Andrey Sitnik <andrey@sitnik.ru>
2023-03-16 11:26:28 +0100

    Typo

--- scripts/check-changes.js
+++ scripts/check-changes.js
@@ -1,4 +1,4 @@
-import { writeSync } from 'node:fs/promises'
+import { writeSync } from 'node:fs'
 import { execSync } from 'node:child_process'
 
 if (execSync('git status pnpm-lock.yaml -s').includes(' pnpm-lock.yaml')) {
