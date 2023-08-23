c31739ca9cb641b919f1be2e9db598f25b3e2983
Andrey Sitnik <andrey@sitnik.ru>
2023-08-23 10:24:28 +0200

    Fix event name

--- client/view/Intro/Intro.js
+++ client/view/Intro/Intro.js
@@ -3,7 +3,7 @@ import { trackEvent } from '../../lib/analytics.js'
 let links = document.querySelectorAll('.Intro_supported a')
 
 for (let link of links) {
-  let event = link.innerHTML.includes('evilmartians')
+  let event = link.href.includes('evilmartians')
     ? 'Open Evil Martians'
     : 'Open Cube'
   link.addEventListener('click', () => {
