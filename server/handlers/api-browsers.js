2ddf71be83997d196e3be8281ee2f47452011e03
Alexander Chabin <nibach@ya.ru>
2023-08-26 06:42:31 +0500

    Add comment why is used `null` instead `alt-ww` by default

--- server/handlers/api-browsers.js
+++ server/handlers/api-browsers.js
@@ -3,7 +3,10 @@ import { sendResponseAPI } from '../lib/send-response.js'
 
 export async function handleAPIBrowsers(req, res, url) {
   let config = url.searchParams.get('q') || 'defaults'
-  let region = url.searchParams.get('region') || 'alt-ww'
+
+  // Specify the default global region as `null`, not `alt-ww`
+  // `caniuse-db` has differences between stats data (https://github.com/Fyrd/caniuse/issues/6811):
+  // `usage_global` in `data.json` is updated daily/weekly, `region-usage-json/alt-ww.json` is updated monthly
   let region = url.searchParams.get('region') || null
 
   try {
