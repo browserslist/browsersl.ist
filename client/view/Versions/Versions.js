1e86df8dc63a624f0687d37053cc2169af7a43d3
Andrey Sitnik <andrey@sitnik.ru>
2023-09-27 00:17:48 +0200

    Add ability to hide/show docs

--- client/view/Versions/Versions.js
+++ client/view/Versions/Versions.js
@@ -1,6 +1,7 @@
 let updated = document.querySelector('[data-id=versions_updated]')
 let browserslist = document.querySelector('[data-id=versions_browserslist]')
 let caniuse = document.querySelector('[data-id=versions_caniuse]')
+let docs = document.querySelector('[data-id=versions_docs]')
 
 export function updateVersions(browserslistVersion, caniuseVersion, updatedMs) {
   updated.innerText = new Date(updatedMs).toLocaleDateString('en-UK', {
@@ -11,3 +12,24 @@ export function updateVersions(browserslistVersion, caniuseVersion, updatedMs) {
   browserslist.innerText = browserslistVersion
   caniuse.innerText = caniuseVersion
 }
+
+let hideResults = docs.innerText
+
+function parseUrl() {
+  if (location.search.includes('?results')) {
+    docs.innerText = 'Show docs'
+    document.body.classList.add('without-docs')
+  } else {
+    docs.innerText = hideResults
+    document.body.classList.remove('without-docs')
+  }
+}
+
+docs.addEventListener('click', () => {
+  let search = location.search.includes('?results') ? '' : '?results'
+  history.pushState(null, null, '/' + search + location.hash)
+  parseUrl()
+})
+
+window.addEventListener('popstate', parseUrl)
+parseUrl()
