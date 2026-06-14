5b6951dd3f4bb0bbf7778953d03b5ba8a6865161
Andrey Sitnik <andrey@sitnik.es>
2026-06-14 21:20:13 +0000

    Update version sync script

--- scripts/check-versions.js
+++ scripts/check-versions.js
@@ -7,15 +7,7 @@ let nodeVersion = readFileSync(join(ROOT, '.node-version')).toString().trim()
 let dockerfile = readFileSync(join(ROOT, 'Dockerfile')).toString()
 let packageJson = readFileSync(join(ROOT, 'package.json')).toString()
 
-let pnpmVersion = packageJson.match(/pnpm@([.\d]+)/)[1]
 let nodeMajor = nodeVersion.match(/^(\d+)\./)[1]
-
-if (!dockerfile.includes(`PNPM_VERSION=${pnpmVersion}`)) {
-  process.stderr.write(
-    'Dockerfile and package.json have different pnpm version\n'
-  )
-  process.exit(1)
-}
 
 if (!dockerfile.includes(`NODE_VERSION=${nodeVersion}`)) {
   process.stderr.write(
